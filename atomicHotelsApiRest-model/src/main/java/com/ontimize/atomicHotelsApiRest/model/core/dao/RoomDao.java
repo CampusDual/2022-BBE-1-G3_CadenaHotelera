5f95b06f36f6ded3aa174f1c1c2f17698b4f4a57
Estefanía Penide <estefaniapenide@gmail.com>
2022-06-28 14:25:16 +0200

    ep nuevo RoomDao.java

---
+++ atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/dao/RoomDao.java
@@ -0,0 +1,24 @@
+package com.ontimize.atomicHotelsApiRest.model.core.dao;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Repository;
+
+import com.ontimize.jee.server.dao.common.ConfigurationFile;
+
+@Repository("RoomDao")
+@Lazy
+@ConfigurationFile(configurationFile = "dao/RoomDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
+public class RoomDao {
+	
+	public static final String TAG = "rm_";
+	public static final String ATTR_HOTEL_ID = TAG +"htl_id";
+	public static final String ATTR_NUMBER = TAG +"number";
+	public static final String ATTR_SQUARE_METERS = TAG +"square_meters";
+	public static final String ATTR_ROOM_TYPE_ID = TAG +"rmt_id";
+	public static final String ATTR_STATUS = TAG +"status";
+
+
+
+}
+
+
