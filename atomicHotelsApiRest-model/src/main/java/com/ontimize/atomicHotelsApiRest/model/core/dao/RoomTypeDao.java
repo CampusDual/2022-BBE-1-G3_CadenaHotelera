bf15ee7dac31825a28a15090002107b6754efa5d
Arlumno <arlumno@gmail.com>
2022-06-29 11:17:00 +0200

    Ar - roomType

--- atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/dao/RoomTypeDao.java
+++ atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/dao/RoomTypeDao.java
@@ -11,10 +11,9 @@ import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
 @ConfigurationFile(configurationFile = "dao/RoomTypeDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
 public class RoomTypeDao extends OntimizeJdbcDaoSupport {
 	public static final String TAG = "rmt_";
-	public static final String ATTR_ID = TAG +"rmt_id";
+	public static final String ATTR_ID = TAG +"id";
 	public static final String ATTR_NAME = TAG +"name";
 	public static final String ATTR_DESCRIPTION = TAG +"description";
 	public static final String ATTR_PRICE = TAG +"price";
-	public static final String ATTR_HOTEL_ID = TAG +"htl_id";
 	public static final String ATTR_BEDS_COMBO = TAG +"bdc_id";	
 }
