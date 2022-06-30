7c2fc2019b1da3e6aaaa94661059f941f78e1231
Estefanía Penide <estefaniapenide@gmail.com>
2022-06-30 12:17:14 +0200

    EP. - HotelDao Tag

--- atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/dao/HotelDao.java
+++ atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/dao/HotelDao.java
@@ -10,17 +10,18 @@ import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
 @Lazy
 @ConfigurationFile(configurationFile = "dao/HotelDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
 public class HotelDao extends OntimizeJdbcDaoSupport {
-
-	public static final String ATTR_ID = "htl_id";
-	public static final String ATTR_NAME = "htl_name";
-	public static final String ATTR_STREET = "htl_street";
-	public static final String ATTR_CITY = "htl_city";
-	public static final String ATTR_CP = "htl_postal_code";
-	public static final String ATTR_STATE = "htl_state";
-	public static final String ATTR_COUNTRY = "htl_country";
-	public static final String ATTR_PHONE = "htl_phone";
-	public static final String ATTR_EMAIL = "htl_email";
-	public static final String ATTR_DESCRIPTION = "htl_description";
-	public static final String ATTR_IS_OPEN = "htl_is_open";
+	
+	public static final String TAG = "htl_";
+	public static final String ATTR_ID = TAG+"id";
+	public static final String ATTR_NAME = TAG+"name";
+	public static final String ATTR_STREET = TAG+"street";
+	public static final String ATTR_CITY = TAG+"city";
+	public static final String ATTR_CP = TAG+"postal_code";
+	public static final String ATTR_STATE = TAG+"state";
+	public static final String ATTR_COUNTRY = TAG+"country";
+	public static final String ATTR_PHONE = TAG+"phone";
+	public static final String ATTR_EMAIL = TAG+"email";
+	public static final String ATTR_DESCRIPTION = TAG+"description";
+	public static final String ATTR_IS_OPEN = TAG+"is_open";
 
 }
