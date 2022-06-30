6984df43b2e90c6ed928c3edcc157e701b609971
cesarbouzas <cesarbouzaspaw@gmail.com>
2022-06-30 13:23:06 +0200

    service_rest

--- atomicHotelsApiRest-ws/src/main/java/com/ontimize/atomicHotelsApiRest/ws/core/rest/ServiceRestController.java
+++ atomicHotelsApiRest-ws/src/main/java/com/ontimize/atomicHotelsApiRest/ws/core/rest/ServiceRestController.java
@@ -4,18 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ontimize.atomicHotelsApiRest.api.core.service.IFeatureService;
+
+import com.ontimize.atomicHotelsApiRest.api.core.service.IServiceService;
 import com.ontimize.jee.server.rest.ORestController;
 
 @RestController // indica que esta clase trabaja como un controlador, que responderá a las peticiones cuya URL tenga el path indicado en la anotación
 @RequestMapping("/services") //(en este caso, services)
-public class ServiceRestController extends ORestController<IFeatureService>{
+public class ServiceRestController extends ORestController<IServiceService>{
 
 	 @Autowired //permite que los DAO se enlacen correctamente a las variables donde las hemos definido, evitando el uso de métodos getter y setter.
-	 private IFeatureService serviceService;
+	 private IServiceService serviceService;
 
 	 @Override
-	 public IFeatureService getService() {
+	 public IServiceService getService() {
 	  return this.serviceService;
 	 }
 
