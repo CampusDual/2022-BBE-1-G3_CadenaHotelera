a1c39d563d81e1e54de4438bad7367a761f4de3f
Estefanía Penide <estefaniapenide@gmail.com>
2022-07-04 08:46:34 +0200

    EP.- RoomService Excepciones

--- atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/service/RoomService.java
+++ atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/service/RoomService.java
@@ -7,13 +7,17 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.SQLWarningException;
 import org.springframework.stereotype.Service;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.atomicHotelsApiRest.api.core.service.IRoomService;
+import com.ontimize.atomicHotelsApiRest.model.core.dao.FeatureDao;
 import com.ontimize.atomicHotelsApiRest.model.core.dao.HotelDao;
 import com.ontimize.atomicHotelsApiRest.model.core.dao.RoomDao;
 import com.ontimize.atomicHotelsApiRest.model.core.dao.RoomTypeDao;
+import com.ontimize.atomicHotelsApiRest.model.core.ontimizeExtra.EntityResultWrong;
 import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.common.tools.EntityResultTools;
@@ -38,47 +42,64 @@ public class RoomService implements IRoomService {
 
 	@Override
 	public EntityResult roomInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
+
 		EntityResult resultado = new EntityResultMapImpl();
 		try {
 			resultado = this.daoHelper.insert(this.roomDao, attrMap);
-			if (resultado != null && resultado.getCode() == EntityResult.OPERATION_WRONG) {
-				resultado.setMessage("Error al insertar datos");
-			} else {
-				resultado.setMessage("Inserción realizada");
-			}
+			resultado.setMessage("Room registrada");
 		} catch (DuplicateKeyException e) {
-			resultado.setCode(EntityResult.OPERATION_WRONG);
-			resultado.setMessage("Error al crear Room - El registro ya existe");
+			resultado = new EntityResultWrong("Error al crear Room - El registro ya existe");
+		} catch (DataIntegrityViolationException e) {
+			resultado = new EntityResultWrong("Error al crear Room - Falta algún campo obligatorio");
+		} catch (Exception e) {
+			resultado = new EntityResultWrong("Error al registrar Room");
 		}
 
 		return resultado;
+
 	}
 
 	@Override
 	public EntityResult roomUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
 			throws OntimizeJEERuntimeException {
-		EntityResult resultado = new EntityResultMapImpl();
-		try {
-			resultado = this.daoHelper.update(this.roomDao, attrMap, keyMap);
-			if (resultado != null && resultado.getCode() == EntityResult.OPERATION_WRONG) {
-				resultado.setMessage("Error al actualizar datos");
-			} else {
-				resultado.setMessage("Actualización realizada");
+			EntityResult resultado = new EntityResultMapImpl();
+			try {
+				resultado = this.daoHelper.update(this.roomDao, attrMap, keyMap);
+				resultado.setMessage("Room actualizada");
+			} catch (DuplicateKeyException e) {
+				resultado = new EntityResultWrong("Error al actualizar Room - No es posible duplicar un registro");
+			} catch (DataIntegrityViolationException e) {
+				resultado = new EntityResultWrong("Error al actualizar Room - No existe el tipo de habitación (FK rm_rmt_id)");
+			} catch (SQLWarningException e) {
+				resultado = new EntityResultWrong(
+						"Error al actualizar Room - Falta el rm_id (PK) de la Room a actualizar");
+			} catch (Exception e) {
+				resultado = new EntityResultWrong("Error al actualizar Room");
 			}
-		} catch (DuplicateKeyException e) {
-			resultado.setCode(EntityResult.OPERATION_WRONG);
-			resultado.setMessage("Error al actualizar Room - No se puede duplicar un registro");
-		} catch (org.springframework.dao.DataIntegrityViolationException e) {
-			resultado.setCode(EntityResult.OPERATION_WRONG);
-			resultado.setMessage("Error al actualizar Room - Prentende asignarle un tipo de habitación que no existe");
-		}
-		return resultado;
+			return resultado; 
 	}
 
 	@Override
 	public EntityResult roomDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
-		EntityResult resultado = new EntityResultMapImpl();
-		resultado = this.daoHelper.delete(this.roomDao, keyMap);
+		
+		EntityResult resultado = new EntityResultMapImpl();		
+		try {
+			if (keyMap.containsKey(RoomDao.ATTR_ID)) {
+				EntityResult auxEntity = this.daoHelper.query(this.roomDao,
+						EntityResultTools.keysvalues(RoomDao.ATTR_ID, keyMap.get(RoomDao.ATTR_ID)),
+						EntityResultTools.attributes(RoomDao.ATTR_ID));
+				if (auxEntity.calculateRecordNumber() == 0) { // si no hay registros...
+					resultado = new EntityResultWrong("Error al eliminar Room - La Room a eliminar no existe");
+				} else {
+					resultado = this.daoHelper.delete(this.roomDao, keyMap);
+					resultado.setMessage("Room eliminada");
+				}
+			}else {
+				resultado = new EntityResultWrong("Error al eliminar Room - Falta el rm_id (PK) de la Room a eliminar");
+			}
+		} catch (Exception e) {
+			resultado = new EntityResultWrong("Error al eliminar Room");
+		}
 		return resultado;
 	}
 
