ea34e50e4ceaa25621cdf8ca006537da2a5186fa
Alex Pernas <51008575+neliuss@users.noreply.github.com>
2022-07-11 11:14:16 +0200

    tabla servicesXtra

--- atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/service/ServicesXtraService.java
+++ atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/service/ServicesXtraService.java
@@ -1,1 +1,104 @@
 package com.ontimize.atomicHotelsApiRest.model.core.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.SQLWarningException;
+import org.springframework.stereotype.Service;
+
+import com.ontimize.atomicHotelsApiRest.api.core.service.IFeatureService;
+import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.dto.EntityResultMapImpl;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+import com.ontimize.jee.common.tools.EntityResultTools;
+import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+import com.ontimize.atomicHotelsApiRest.model.core.dao.FeatureDao;
+import com.ontimize.atomicHotelsApiRest.model.core.dao.HotelDao;
+import com.ontimize.atomicHotelsApiRest.model.core.dao.ServicesXtraDao;
+import com.ontimize.atomicHotelsApiRest.model.core.ontimizeExtra.EntityResultWrong;
+
+@Service("FeatureService")
+@Lazy
+public class ServicesXtraService implements IFeatureService{
+	
+	@Autowired
+	private ServicesXtraDao servicesXtraDao;
+	@Autowired
+	private DefaultOntimizeDaoHelper daoHelper;
+
+	@Override
+	public EntityResult featureQuery(Map<String, Object> keyMap, List<String> attrList)
+			throws OntimizeJEERuntimeException {
+		EntityResult resultado = this.daoHelper.query(this.servicesXtraDao, keyMap, attrList);
+		return resultado;
+	}
+
+	@Override
+	public EntityResult featureInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
+		
+		EntityResult resultado = new EntityResultMapImpl();
+		try {
+			resultado = this.daoHelper.insert(this.servicesXtraDao, attrMap);
+			resultado.setMessage("Servicio extra registrado correctamente.");
+		} catch (DuplicateKeyException e) {
+			resultado = new EntityResultWrong("Error al crear el servicio extra - El servicio ya existe.");
+		} catch (DataIntegrityViolationException e) {
+			resultado = new EntityResultWrong("Error al crear el servicio extra - Falta algún campo obligatorio.");
+		} catch (Exception e) {
+			resultado = new EntityResultWrong("Error al registrar el servicio extra.");
+		}
+
+		return resultado;
+	}
+
+	@Override
+	public EntityResult featureUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
+			throws OntimizeJEERuntimeException {
+		EntityResult resultado = new EntityResultMapImpl();
+		try {
+			resultado = this.daoHelper.update(this.servicesXtraDao, attrMap, keyMap);
+			resultado.setMessage("Servicio extra actualizada");
+		} catch (DuplicateKeyException e) {
+			resultado = new EntityResultWrong("Error al actualizar el servicio extra - No es posible duplicar un registro.");
+		} catch (DataIntegrityViolationException e) {
+			resultado = new EntityResultWrong("Error al actualizar el servicio extra - Falta algún campo obligatorio.");
+		} catch (SQLWarningException e) {
+			resultado = new EntityResultWrong(
+					"Error al actualizar el servicio extra - Falta el sxt_id (PK) del servicio extra a actualizar.");
+		} catch (Exception e) {
+			resultado = new EntityResultWrong("Error al actualizar el servicio extra.");
+		}
+		return resultado; 
+	}
+
+	@Override
+	public EntityResult featureDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
+		
+		EntityResult resultado = new EntityResultMapImpl();		
+		try {
+			if (keyMap.containsKey(FeatureDao.ATTR_ID)) {
+				EntityResult auxEntity = this.daoHelper.query(this.servicesXtraDao,
+						EntityResultTools.keysvalues(FeatureDao.ATTR_ID, keyMap.get(FeatureDao.ATTR_ID)),
+						EntityResultTools.attributes(FeatureDao.ATTR_ID));
+				if (auxEntity.calculateRecordNumber() == 0) { // si no hay registros...
+					resultado = new EntityResultWrong("Error al eliminar el servicio extra - El servicio extra a eliminar no existe");
+				} else {
+					resultado = this.daoHelper.delete(this.servicesXtraDao, keyMap);
+					resultado.setMessage("Servicio extra eliminado");
+				}
+			}else {
+				resultado = new EntityResultWrong("Error al eliminar el servicio extra - Falta el sxt_id (PK) del servicio extra a eliminar");
+			}
+		} catch (Exception e) {
+			resultado = new EntityResultWrong("Error al eliminar el servicio extra");
+		}
+		return resultado;
+	}
+
+}
---
+++ atomicHotelsApiRest-ws/src/main/java/com/ontimize/atomicHotelsApiRest/ws/core/rest/ServicesXtraRestController.java
@@ -0,0 +1,23 @@
+package com.ontimize.atomicHotelsApiRest.ws.core.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ontimize.atomicHotelsApiRest.api.core.service.ICustomerService;
+import com.ontimize.jee.server.rest.ORestController;
+
+@RestController // indica que esta clase trabaja como un controlador, que responderá a las peticiones cuya URL tenga el path indicado en la anotación
+@RequestMapping("/servicesXtra") //(en este caso, servicesXtra)
+
+public class ServicesXtraRestController extends ORestController<ICustomerService>{
+
+	 @Autowired //permite que los DAO se enlacen correctamente a las variables donde las hemos definido, evitando el uso de métodos getter y setter.
+	 private ICustomerService servicesXtraService;
+
+	 @Override
+	 public ICustomerService getService() {
+	  return this.servicesXtraService;
+	 }
+
+}
