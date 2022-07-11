2afcbd75e94a0e14a11be15788a815a954d2c710
Alex Pernas <51008575+neliuss@users.noreply.github.com>
2022-07-11 12:11:00 +0200

    cambios dao

--- atomicHotelsApiRest-ws/src/main/java/com/ontimize/atomicHotelsApiRest/ws/core/rest/ServicesXtraRestController.java
+++ atomicHotelsApiRest-ws/src/main/java/com/ontimize/atomicHotelsApiRest/ws/core/rest/ServicesXtraRestController.java
@@ -4,19 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ontimize.atomicHotelsApiRest.api.core.service.ICustomerService;
+import com.ontimize.atomicHotelsApiRest.api.core.service.IServicesXtraService;
 import com.ontimize.jee.server.rest.ORestController;
 
 @RestController // indica que esta clase trabaja como un controlador, que responderá a las peticiones cuya URL tenga el path indicado en la anotación
 @RequestMapping("/servicesXtra") //(en este caso, servicesXtra)
 
-public class ServicesXtraRestController extends ORestController<ICustomerService>{
+public class ServicesXtraRestController extends ORestController<IServicesXtraService>{
 
 	 @Autowired //permite que los DAO se enlacen correctamente a las variables donde las hemos definido, evitando el uso de métodos getter y setter.
-	 private ICustomerService servicesXtraService;
+	 private IServicesXtraService servicesXtraService;
 
 	 @Override
-	 public ICustomerService getService() {
+	 public IServicesXtraService getService() {
 	  return this.servicesXtraService;
 	 }
 
