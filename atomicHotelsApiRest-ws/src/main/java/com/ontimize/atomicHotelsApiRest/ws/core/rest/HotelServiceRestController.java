025f46d634392f7b482274b753953d0db9e54245
Estefanía Penide <estefaniapenide@gmail.com>
2022-07-11 12:39:23 +0200

    EP.- Control Errores

--- atomicHotelsApiRest-ws/src/main/java/com/ontimize/atomicHotelsApiRest/ws/core/rest/HotelServiceRestController.java
+++ atomicHotelsApiRest-ws/src/main/java/com/ontimize/atomicHotelsApiRest/ws/core/rest/HotelServiceRestController.java
@@ -7,7 +7,7 @@ import com.ontimize.atomicHotelsApiRest.api.core.service.IHotelServiceService;
 import com.ontimize.jee.server.rest.ORestController;
 
 @RestController 
-@RequestMapping("/hotelserviceservice") 
+@RequestMapping("/hotelservice") 
 public class HotelServiceRestController extends ORestController<IHotelServiceService>{
 
 	 @Autowired //permite que los DAO se enlacen correctamente a las variables donde las hemos definido, evitando el uso de métodos getter y setter.
