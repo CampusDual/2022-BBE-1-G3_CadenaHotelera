5a6bf1ef4cfb4726f121dc87feee1985d441164e
DESKTOP-UEHN76G\Tomás <tgillage@gmail.com>
2022-07-12 10:14:25 +0200

    "update TGL"

--- atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/dao/HotelServiceExtraDao.java
+++ atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/dao/HotelServiceExtraDao.java
@@ -6,14 +6,11 @@ import org.springframework.stereotype.Repository;
 import com.ontimize.jee.server.dao.common.ConfigurationFile;
 import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
 
-public class HotelServiceExtraDao extends OntimizeJdbcDaoSupport  {
-
-	@Repository("HotelServiceDao")
-	@Lazy
-	@ConfigurationFile(configurationFile = "dao/HotelServiceExtraDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
-	public class HotelServiceDao extends OntimizeJdbcDaoSupport {
+@Repository("HotelServiceExtraDao")
+@Lazy
+@ConfigurationFile(configurationFile = "dao/HotelServiceExtraDao.xml", configurationFilePlaceholder = "dao/placeholders.properties")
+public class HotelServiceExtraDao extends OntimizeJdbcDaoSupport {
 		public static final String ATTR_ID_HTL = "htl_id";
 		public static final String ATTR_ID_SXT = "sxt_id";
-	}
 
 }
