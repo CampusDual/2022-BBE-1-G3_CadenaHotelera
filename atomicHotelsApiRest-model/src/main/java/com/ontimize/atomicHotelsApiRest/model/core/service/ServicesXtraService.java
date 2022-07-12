70c127b0698916a87ef4a6efb1e1a5f9a1caf2e3
Estefanía Penide <estefaniapenide@gmail.com>
2022-07-12 14:39:37 +0200

    EP.- Errores ServicesXtra

--- atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/service/ServicesXtraService.java
+++ atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/service/ServicesXtraService.java
@@ -20,10 +20,13 @@ import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.common.tools.EntityResultTools;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+import com.ontimize.atomicHotelsApiRest.model.core.dao.CustomerDao;
 import com.ontimize.atomicHotelsApiRest.model.core.dao.FeatureDao;
 import com.ontimize.atomicHotelsApiRest.model.core.dao.HotelDao;
+import com.ontimize.atomicHotelsApiRest.model.core.dao.RoomTypeDao;
 import com.ontimize.atomicHotelsApiRest.model.core.dao.ServicesXtraDao;
 import com.ontimize.atomicHotelsApiRest.model.core.tools.EntityResultWrong;
+import com.ontimize.atomicHotelsApiRest.model.core.tools.ErrorMessage;
 import com.ontimize.atomicHotelsApiRest.model.core.tools.ValidateFields;
 
 @Service("ServicesXtraService")
@@ -47,43 +50,45 @@ public class ServicesXtraService implements IServicesXtraService{
 		
 		EntityResult resultado = new EntityResultMapImpl();
 		try {
-
-	//		ValidateFields.required(attrMap, ServicesXtraDao.ATTR_DESCRIPTION);
-			ValidateFields.emptyField(attrMap, ServicesXtraDao.ATTR_NAME);
-		//	ValidateFields.emptyFields(attrMap);
+			ValidateFields.required(attrMap, ServicesXtraDao.ATTR_NAME);
+			
 			resultado = this.daoHelper.insert(this.servicesXtraDao, attrMap);
-			resultado.setMessage("Servicio extra registrado correctamente.");
-		} catch (DuplicateKeyException e) {
-			resultado = new EntityResultWrong("Error al crear el servicio extra - El servicio ya existe.");
-		} catch (DataIntegrityViolationException e) {
-			resultado = new EntityResultWrong("Error al crear el servicio extra - Falta algún campo obligatorio.");
-		} catch (MissingFieldsException e) {
-			resultado = new EntityResultWrong(e.getMessage());
-		}
-		catch (Exception e) {
-			resultado = new EntityResultWrong("Error al registrar el servicio extra.");
-		}
+			resultado.setMessage("ServiceXtra registrado");
 
+		} catch (MissingFieldsException e) {
+			resultado = new EntityResultWrong(ErrorMessage.CREATION_ERROR + e.getMessage());	
+			
+		} catch (DuplicateKeyException e) {
+			resultado = new EntityResultWrong(ErrorMessage.CREATION_ERROR_DUPLICATED_FIELD);
+
+		} catch (Exception e) {
+			resultado = new EntityResultWrong(ErrorMessage.CREATION_ERROR);
+		}
+		
 		return resultado;
 	}
 
 	@Override
 	public EntityResult servicesXtraUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
 			throws OntimizeJEERuntimeException {
+		
 		EntityResult resultado = new EntityResultMapImpl();
 		try {
+			ValidateFields.required(keyMap, ServicesXtraDao.ATTR_ID);
 			resultado = this.daoHelper.update(this.servicesXtraDao, attrMap, keyMap);
-			resultado.setMessage("Servicio extra actualizada");
+			if (resultado.getCode() == EntityResult.OPERATION_SUCCESSFUL_SHOW_MESSAGE) {
+				resultado = new EntityResultWrong(ErrorMessage.UPDATE_ERROR_MISSING_FIELD);
+			} else {
+				resultado.setMessage("ServiceXtra actualizado");
+			}
+		} catch (MissingFieldsException e) {
+			resultado = new EntityResultWrong(ErrorMessage.UPDATE_ERROR + e.getMessage());
 		} catch (DuplicateKeyException e) {
-			resultado = new EntityResultWrong("Error al actualizar el servicio extra - No es posible duplicar un registro.");
+			resultado = new EntityResultWrong(ErrorMessage.UPDATE_ERROR_DUPLICATED_FIELD);
 		} catch (DataIntegrityViolationException e) {
-			resultado = new EntityResultWrong("Error al actualizar el servicio extra - Falta algún campo obligatorio.");
-		} catch (SQLWarningException e) { 
-			e.printStackTrace();
-			resultado = new EntityResultWrong(
-					"Error al actualizar el servicio extra - Falta el sxt_id (PK) del servicio extra a actualizar.");
+			resultado = new EntityResultWrong(ErrorMessage.UPDATE_ERROR_REQUIRED_FIELDS);
 		} catch (Exception e) {
-			resultado = new EntityResultWrong("Error al actualizar el servicio extra.");
+			resultado = new EntityResultWrong(ErrorMessage.UPDATE_ERROR);
 		}
 		return resultado; 
 	}
@@ -91,25 +96,28 @@ public class ServicesXtraService implements IServicesXtraService{
 	@Override
 	public EntityResult servicesXtraDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
 		
-		EntityResult resultado = new EntityResultMapImpl();		
+		EntityResult resultado = new EntityResultMapImpl();
 		try {
-			if (keyMap.containsKey(ServicesXtraDao.ATTR_ID)) {
-				EntityResult auxEntity = this.daoHelper.query(this.servicesXtraDao,
-						EntityResultTools.keysvalues(ServicesXtraDao.ATTR_ID, keyMap.get(ServicesXtraDao.ATTR_ID)),
-						EntityResultTools.attributes(ServicesXtraDao.ATTR_ID));
-				if (auxEntity.calculateRecordNumber() == 0) { // si no hay registros...
-					resultado = new EntityResultWrong("Error al eliminar el servicio extra - El servicio extra a eliminar no existe");
-				} else {
-					resultado = this.daoHelper.delete(this.servicesXtraDao, keyMap);
-					resultado.setMessage("Servicio extra eliminado");
-				}
-			}else {
-				resultado = new EntityResultWrong("Error al eliminar el servicio extra - Falta el sxt_id (PK) del servicio extra a eliminar");
+			ValidateFields.required(keyMap, ServicesXtraDao.ATTR_ID);
+
+			EntityResult auxEntity = this.daoHelper.query(this.servicesXtraDao,
+					EntityResultTools.keysvalues(ServicesXtraDao.ATTR_ID, keyMap.get(ServicesXtraDao.ATTR_ID)),
+					EntityResultTools.attributes(ServicesXtraDao.ATTR_ID));
+			if (auxEntity.calculateRecordNumber() == 0) { // si no hay registros...
+				resultado = new EntityResultWrong(ErrorMessage.DELETE_ERROR_MISSING_FIELD);
+			} else {
+				resultado = this.daoHelper.delete(this.servicesXtraDao, keyMap);
+				resultado.setMessage("ServiceXtra eliminado");
 			}
+		} catch (MissingFieldsException e) {
+			resultado = new EntityResultWrong(ErrorMessage.DELETE_ERROR + e.getMessage());
+		} catch (DataIntegrityViolationException e) {
+			resultado = new EntityResultWrong(ErrorMessage.DELETE_ERROR_FOREING_KEY);
 		} catch (Exception e) {
-			resultado = new EntityResultWrong("Error al eliminar el servicio extra");
+			resultado = new EntityResultWrong(ErrorMessage.DELETE_ERROR);
 		}
 		return resultado;
+		
 	}
 
 }
