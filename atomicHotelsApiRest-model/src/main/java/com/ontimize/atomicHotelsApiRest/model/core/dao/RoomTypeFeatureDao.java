26fb3928dda5d497411eac0998d48fdd37f27f78
Alex Pernas <51008575+neliuss@users.noreply.github.com>
2022-07-27 09:48:31 +0200

    room type feature

--- atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/dao/RoomTypeFeatureDao.java
+++ atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/dao/RoomTypeFeatureDao.java
@@ -1,8 +1,12 @@
 package com.ontimize.atomicHotelsApiRest.model.core.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Repository;
 
+import com.ontimize.atomicHotelsApiRest.model.core.tools.TypeCodes.type;
 import com.ontimize.jee.server.dao.common.ConfigurationFile;
 import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
 
@@ -12,4 +16,11 @@ import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
 public class RoomTypeFeatureDao extends OntimizeJdbcDaoSupport {
 	public static final String ATTR_ROOM_ID = "rmt_id";
 	public static final String ATTR_FEATURE_ID = "ftr_id";
+	
+	public static final Map<String,type> fields = new HashMap<>() {{
+		put(ATTR_ROOM_ID,type.INTEGER);
+		put(ATTR_FEATURE_ID,type.INTEGER);
+				
+	}};
+	
 }
--- atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/service/RoomTypeFeatureService.java
+++ atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/service/RoomTypeFeatureService.java
@@ -12,6 +12,7 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 
 import com.ontimize.atomicHotelsApiRest.api.core.exceptions.MissingFieldsException;
+import com.ontimize.atomicHotelsApiRest.api.core.exceptions.ValidateException;
 import com.ontimize.atomicHotelsApiRest.api.core.service.IFeatureService;
 import com.ontimize.atomicHotelsApiRest.api.core.service.IRoomTypeFeatureService;
 import com.ontimize.jee.common.dto.EntityResult;
@@ -19,10 +20,12 @@ import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.common.tools.EntityResultTools;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+import com.ontimize.atomicHotelsApiRest.model.core.dao.CreditCardDao;
 import com.ontimize.atomicHotelsApiRest.model.core.dao.FeatureDao;
 import com.ontimize.atomicHotelsApiRest.model.core.dao.HotelDao;
 import com.ontimize.atomicHotelsApiRest.model.core.dao.RoomTypeDao;
 import com.ontimize.atomicHotelsApiRest.model.core.dao.RoomTypeFeatureDao;
+import com.ontimize.atomicHotelsApiRest.model.core.tools.ControlFields;
 import com.ontimize.atomicHotelsApiRest.model.core.tools.EntityResultWrong;
 import com.ontimize.atomicHotelsApiRest.model.core.tools.ErrorMessage;
 import com.ontimize.atomicHotelsApiRest.model.core.tools.ValidateFields;
@@ -39,15 +42,71 @@ public class RoomTypeFeatureService implements IRoomTypeFeatureService{
 	@Override
 	public EntityResult roomTypeFeatureQuery(Map<String, Object> keyMap, List<String> attrList)
 			throws OntimizeJEERuntimeException {
-		  return this.daoHelper.query(this.roomTypeFeatureDao, keyMap, attrList);		
+		EntityResult resultado = new EntityResultWrong();
+
+		try {
+
+			// Control del filtro
+			ControlFields cf = new ControlFields();
+			cf.addBasics(RoomTypeFeatureDao.fields);
+//			cf.setOptional(true);//El resto de los campos de fields serán aceptados, por defecto true			
+			cf.validate(keyMap);
+
+			cf.validate(attrList);// reutilizamos los mismos criterios para validar attrList
+
+			resultado = this.daoHelper.query(this.roomTypeFeatureDao, keyMap, attrList);
+
+		} catch (ValidateException e) {
+			e.printStackTrace();
+			resultado = new EntityResultWrong(e.getMessage());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			resultado = new EntityResultWrong(ErrorMessage.ERROR);
+		}
+		return resultado;		
 	}
+	
+	
 	@Override
 	public EntityResult roomTypeFeatureInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException {
 		
-		EntityResult resultado = new EntityResultMapImpl();
+		EntityResult resultado = new EntityResultWrong();
 		try {
 			
-			ValidateFields.required(attrMap, RoomTypeFeatureDao.ATTR_FEATURE_ID, RoomTypeFeatureDao.ATTR_ROOM_ID);	
+			ControlFields cf = new ControlFields();
+			List<String> required = new ArrayList<String>() {
+				{
+					add(RoomTypeFeatureDao.ATTR_ROOM_ID);
+					add(RoomTypeFeatureDao.ATTR_FEATURE_ID);
+				}
+			};
+/*			List<String> restricted = new ArrayList<String>() {
+				{
+					add(RoomTypeFeatureDao.ATTR_ROOM_ID);// No quiero que meta el id porque quiero el id autogenerado de la base de
+					add(RoomTypeFeatureDao.ATTR_FEATURE_ID);	// datos
+				}
+			};
+*/
+			cf.addBasics(RoomTypeFeatureDao.fields);
+			cf.setRequired(required);
+//			cf.setRestricted(restricted);
+//			cf.setOptional(true);//No existen más campos. No es neceario ponerlo
+			cf.validate(attrMap);
+
+			resultado = this.daoHelper.insert(this.roomTypeFeatureDao, attrMap);
+			resultado.setMessage("Room type features registered");
+
+		} catch (ValidateException e) {
+			resultado = new EntityResultWrong(ErrorMessage.CREATION_ERROR + e.getMessage());
+		} catch (DuplicateKeyException e) {
+			resultado = new EntityResultWrong(ErrorMessage.CREATION_ERROR_DUPLICATED_FIELD);
+		} catch (Exception e) {
+			e.printStackTrace();
+			resultado = new EntityResultWrong(ErrorMessage.CREATION_ERROR);
+		}
+
+/*			ValidateFields.required(attrMap, RoomTypeFeatureDao.ATTR_FEATURE_ID, RoomTypeFeatureDao.ATTR_ROOM_ID);	
 			resultado = this.daoHelper.insert(this.roomTypeFeatureDao, attrMap);	
 			resultado.setMessage("RoomTypeFeature registrada");
 
@@ -60,7 +119,7 @@ public class RoomTypeFeatureService implements IRoomTypeFeatureService{
 		} catch (Exception e) {
 			resultado = new EntityResultWrong(ErrorMessage.CREATION_ERROR);
 		}
-		
+*/		
 		return resultado;
 	}
 	
@@ -68,7 +127,44 @@ public class RoomTypeFeatureService implements IRoomTypeFeatureService{
 	@Override
 	public EntityResult roomTypeFeatureDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
 		
-		EntityResult resultado = new EntityResultMapImpl();
+		EntityResult resultado = new EntityResultWrong();
+
+		try {
+			List<String> required = new ArrayList<String>() {
+				{
+					add(RoomTypeFeatureDao.ATTR_ROOM_ID);
+				}
+			};
+			ControlFields cf = new ControlFields();
+			cf.addBasics(RoomTypeFeatureDao.fields);
+			cf.setRequired(required);
+			cf.setOptional(false);
+			cf.validate(keyMap);
+
+			Map<String, Object> consultaKeyMap = new HashMap<>() {
+				{
+					put(RoomTypeFeatureDao.ATTR_ROOM_ID, keyMap.get(RoomTypeFeatureDao.ATTR_ROOM_ID));
+				}
+			};
+
+			EntityResult auxEntity = roomTypeFeatureQuery(consultaKeyMap, EntityResultTools.attributes(RoomTypeFeatureDao.ATTR_ROOM_ID));
+
+			if (auxEntity.calculateRecordNumber() == 0) { // si no hay registros...
+				resultado = new EntityResultWrong(ErrorMessage.DELETE_ERROR_MISSING_FIELD);
+			} else {
+				resultado = this.daoHelper.delete(this.roomTypeFeatureDao, keyMap);
+				resultado.setMessage("Room type features deleted");
+			}
+
+		} catch (ValidateException e) {
+			resultado = new EntityResultWrong(ErrorMessage.DELETE_ERROR + e.getMessage());
+		}  catch (Exception e) {
+			e.printStackTrace();
+			resultado = new EntityResultWrong(ErrorMessage.DELETE_ERROR);
+		}
+		return resultado;
+	}	
+/*		EntityResult resultado = new EntityResultMapImpl();
 		try {
 			ValidateFields.required(keyMap, RoomTypeFeatureDao.ATTR_FEATURE_ID,RoomTypeFeatureDao.ATTR_ROOM_ID);
 
@@ -89,5 +185,5 @@ public class RoomTypeFeatureService implements IRoomTypeFeatureService{
 		}
 		return resultado;
 	}
-
+*/
 }
