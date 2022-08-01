8cf3b617c11b78211cf763c8d49a5a1a4ac29daf
arlumno <arlumno@gmail.com>
2022-08-01 14:41:51 +0200

    Ar -

--- atomicHotelsApiRest-model/src/test/java/com/ontimize/atomicHotelsApiRest/model/core/service/CustomerServiceTest.java
+++ atomicHotelsApiRest-model/src/test/java/com/ontimize/atomicHotelsApiRest/model/core/service/CustomerServiceTest.java
@@ -8,6 +8,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.description;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 
 import java.util.ArrayList;
@@ -34,11 +35,14 @@ import com.ontimize.atomicHotelsApiRest.api.core.exceptions.MissingFieldsExcepti
 import com.ontimize.atomicHotelsApiRest.api.core.exceptions.RestrictedFieldException;
 import com.ontimize.atomicHotelsApiRest.model.core.dao.CustomerDao;
 import com.ontimize.atomicHotelsApiRest.model.core.tools.ControlFields;
+import com.ontimize.atomicHotelsApiRest.model.core.tools.EntityResultWrong;
 import com.ontimize.atomicHotelsApiRest.model.core.tools.ErrorMessage;
+import com.ontimize.jee.common.db.Entity;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+
 @ExtendWith(MockitoExtension.class)
 class CustomerServiceTest {
 
@@ -55,7 +59,7 @@ class CustomerServiceTest {
 	CustomerDao dao;
 
 	EntityResult eR;
-	
+
 	@Nested
 	@DisplayName("Test for Customers queries")
 	@TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -89,11 +93,12 @@ class CustomerServiceTest {
 			eR = service.customerQuery(TestingTools.getMapEmpty(), getColumsName());
 			assertEquals(EntityResult.OPERATION_SUCCESSFUL, eR.getCode(), eR.getMessage());
 
-			// válido: HashMap con filtro que existe 
+			// válido: HashMap con filtro que existe
 			eR = service.customerQuery(getMapId(), getColumsName());
 			assertEquals(EntityResult.OPERATION_SUCCESSFUL, eR.getCode(), eR.getMessage());
 
 		}
+
 		@Test
 		@DisplayName("Valores de entrada NO válidos")
 		void testCustomerQueryKO() {
@@ -131,115 +136,246 @@ class CustomerServiceTest {
 			}
 
 		}
-		
+
 	}
+
 	@Nested
 	@DisplayName("Test for Customers evaluation booleans")
 	@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 	public class CustomerQueryBooleans {
 		@Test
-		@DisplayName("Valores de entrada válidos")
+		@DisplayName("ValidBookingHolder - Valores de salida válidos")
 		void testIsCustomerValidBookingHolderOK() {
 			boolean resultado;
-			
+
 			try {
-				doReturn(TestingTools.getEntityOneRecord()).when(daoHelper).query(any(), anyMap(), anyList(), anyString());
+				doReturn(TestingTools.getEntityOneRecord()).when(daoHelper).query(any(), anyMap(), anyList(),
+						anyString());
 				resultado = service.isCustomerValidBookingHolder(999);
 				assertTrue(resultado);
-				
+
 				doReturn(new EntityResultMapImpl()).when(daoHelper).query(any(), anyMap(), anyList(), anyString());
 				resultado = service.isCustomerValidBookingHolder(999);
 				assertFalse(resultado);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				fail(ErrorMessage.UNCAUGHT_EXCEPTION + e.getMessage());
+			}
+		}
+
+		@Test
+		@DisplayName("ValidBookingHolder - Valores de salida NO válidos - error consulta")
+		void testIsCustomerValidBookingHolderKO() {
+			boolean resultado;			
+			try {
+				doReturn(new EntityResultWrong()).when(daoHelper).query(any(), anyMap(), anyList(), anyString());				
+				assertThrows(EntityResultRequiredException.class, ()-> service.isCustomerValidBookingHolder(999));
+				
+				eR = TestingTools.getEntityOneRecord();
+				eR.setCode(EntityResult.OPERATION_WRONG);
+				doReturn(eR).when(daoHelper).query(any(), anyMap(), anyList(), anyString());				
+				assertThrows(EntityResultRequiredException.class, ()-> service.isCustomerValidBookingHolder(999));		
 				
 			} catch (Exception e) {
 				e.printStackTrace();
 				fail(ErrorMessage.UNCAUGHT_EXCEPTION + e.getMessage());
 			}
-		}		
-		
+		}
+
+		@DisplayName("BloquedQuery - Valores de salida válidos")
+		void testIsCustomerBloquedQueryOK() {
+			boolean resultado;
+
+			try {
+				doReturn(TestingTools.getEntityOneRecord()).when(daoHelper).query(any(), anyMap(), anyList(),
+						anyString());
+				resultado = service.isCustomerBloquedQuery(999);
+				assertFalse(resultado);
+
+				doReturn(new EntityResultMapImpl()).when(daoHelper).query(any(), anyMap(), anyList(), anyString());
+				resultado = service.isCustomerBloquedQuery(999);
+				assertTrue(resultado);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				fail(ErrorMessage.UNCAUGHT_EXCEPTION + e.getMessage());
+			}
+		}
+
+		@Test
+		@DisplayName("BloquedQuery - Valores de salida NO válidos - error consulta")
+		void testIsCustomerBloquedQueryKO() {
+			boolean resultado;
+			try {
+				doReturn(new EntityResultWrong()).when(daoHelper).query(any(), anyMap(), anyList(), anyString());
+				assertThrows(EntityResultRequiredException.class, () -> service.isCustomerBloquedQuery(999));
+
+				eR = TestingTools.getEntityOneRecord();
+				eR.setCode(EntityResult.OPERATION_WRONG);
+				doReturn(eR).when(daoHelper).query(any(), anyMap(), anyList(), anyString());
+				assertThrows(EntityResultRequiredException.class, () -> service.isCustomerBloquedQuery(999));
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				fail(ErrorMessage.UNCAUGHT_EXCEPTION + e.getMessage());
+			}
+		}
+
+	}
+
+	@Nested
+	@DisplayName("Test for Customer inserts")
+	@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+	public class CustomerInsert {
+
+		@Test
+		@DisplayName("ControlFields usar reset()")
+		void testcustomerInsertControlFieldsReset() {
+			service.businessCustomerInsert(TestingTools.getMapEmpty());
+			verify(cf, description("No se ha utilizado el metodo reset de ControlFields")).reset();
+		}
+
+		@Test
+		@DisplayName("ControlFields usar validate() map ")
+		void testCustomerInsertControlFieldsValidate() {
+			service.businessCustomerInsert(TestingTools.getMapEmpty());
+			try {
+				verify(cf, description("No se ha utilizado el metodo validate de ControlFields")).validate(anyMap());
+			} catch (Exception e) {
+				e.printStackTrace();
+				fail(ErrorMessage.UNCAUGHT_EXCEPTION + e.getMessage());
+
+			}
+		}
+
+		@Test
+		@DisplayName("Valores de entrada válidos")
+		void testcustomerInsertOK() {
+			doReturn(new EntityResultMapImpl()).when(daoHelper).insert(any(), anyMap());
+
+			// válido: HashMap campos mínimos
+			eR = service.businessCustomerInsert(getMapRequiredInsert());
+			assertEquals(EntityResult.OPERATION_SUCCESSFUL, eR.getCode(), eR.getMessage());
+
+			// válido: HashMap campos mínimos y mas
+			eR = service.businessCustomerInsert(getMapRequiredInsertExtended());
+			assertEquals(EntityResult.OPERATION_SUCCESSFUL, eR.getCode(), eR.getMessage());
+
+		}
+
 		@Test
 		@DisplayName("Valores de entrada NO válidos")
-		void testIsCustomerValidBookingHolderKO() {
-			boolean resultado;
-			
+		void testcustomerInsertKO() {
 			try {
-				doReturn(TestingTools.getEntityOneRecord()).when(daoHelper).query(any(), anyMap(), anyList(), anyString());
-				resultado = service.isCustomerValidBookingHolder(999);
-				assertTrue(resultado);
-				
-				doReturn(new EntityResultMapImpl()).when(daoHelper).query(any(), anyMap(), anyList(), anyString());
-				resultado = service.isCustomerValidBookingHolder(999);
-				assertFalse(resultado);
-				
+				// lanzamos todas las excepciones de Validate para comprobar que están bien
+				// recojidas.
+				doThrow(MissingFieldsException.class).when(cf).validate(anyMap());
+				eR = service.businessCustomerInsert(TestingTools.getMapEmpty());
+				assertEquals(EntityResult.OPERATION_WRONG, eR.getCode(), eR.getMessage());
+				assertNotEquals(ErrorMessage.UNKNOWN_ERROR, eR.getMessage(), eR.getMessage());
+
+				doThrow(RestrictedFieldException.class).when(cf).validate(anyMap());
+				eR = service.businessCustomerInsert(TestingTools.getMapEmpty());
+				assertEquals(EntityResult.OPERATION_WRONG, eR.getCode(), eR.getMessage());
+				assertNotEquals(ErrorMessage.UNKNOWN_ERROR, eR.getMessage(), eR.getMessage());
+
+				doThrow(InvalidFieldsException.class).when(cf).validate(anyMap());
+				eR = service.businessCustomerInsert(TestingTools.getMapEmpty());
+				assertEquals(EntityResult.OPERATION_WRONG, eR.getCode(), eR.getMessage());
+				assertNotEquals(ErrorMessage.UNKNOWN_ERROR, eR.getMessage(), eR.getMessage());
+
+				doThrow(InvalidFieldsValuesException.class).when(cf).validate(anyMap());
+				eR = service.businessCustomerInsert(TestingTools.getMapEmpty());
+				assertEquals(EntityResult.OPERATION_WRONG, eR.getCode(), eR.getMessage());
+				assertNotEquals(ErrorMessage.UNKNOWN_ERROR, eR.getMessage(), eR.getMessage());
+
+				doThrow(LiadaPardaException.class).when(cf).validate(anyMap());
+				eR = service.businessCustomerInsert(TestingTools.getMapEmpty());
+				assertEquals(EntityResult.OPERATION_WRONG, eR.getCode(), eR.getMessage());
+				assertEquals(ErrorMessage.UNKNOWN_ERROR, eR.getMessage(), eR.getMessage());
+
+				reset(cf);
+				// extra para controlar required:
+				eR = service.businessCustomerInsert(TestingTools.getMapEmpty());
+				assertEquals(EntityResult.OPERATION_WRONG, eR.getCode(), eR.getMessage());
+				assertNotEquals(ErrorMessage.UNKNOWN_ERROR, eR.getMessage(), eR.getMessage());
+				System.out.println(eR.getMessage());
+				assertFalse(eR.getMessage().isEmpty(), eR.getMessage());
+
+				// extra para controlar restricted:
+				eR = service.businessCustomerInsert(getMapRequiredInsertExtendedWidthRestricted());
+				assertEquals(EntityResult.OPERATION_WRONG, eR.getCode(), eR.getMessage());
+				assertNotEquals(ErrorMessage.UNKNOWN_ERROR, eR.getMessage(), eR.getMessage());
+				System.out.println(eR.getMessage());
+				assertFalse(eR.getMessage().isEmpty(), eR.getMessage());
+
 			} catch (Exception e) {
 				e.printStackTrace();
 				fail(ErrorMessage.UNCAUGHT_EXCEPTION + e.getMessage());
 			}
-		}		
+
+		}
 	}
-	
-	
 	// datos entrada
 
-		Map<String, Object> getMapRequiredInsert() {
-			return new HashMap<>() {
-				{
-					put(dao.ATTR_NAME, "Hotel 23");
-					put(dao.ATTR_CITY, "Vigo");
-					put(dao.ATTR_STATE, "Galicia");
-					put(dao.ATTR_COUNTRY, "ES");
-				}
-			};
-		}
-
-		Map<String, Object> getMapUpdate() {
-			return getMapRequiredInsert();
-		}
-
-		
-
-		Map<String, Object> getMapRequiredInsertExtended() {
-
-			return new HashMap<>() {
-				{
-					put(dao.ATTR_NAME, "Hotel 23");
-					put(dao.ATTR_CITY, "Vigo");
-					put(dao.ATTR_STATE, "Galicia");
-					put(dao.ATTR_COUNTRY, "ES");
-					put(dao.ATTR_PHONE, "+34 986 111 111");
-					put(dao.ATTR_EMAIL, "hotel1@atomicHotels.com");
-				}
-			};
-		}
-
-		Map<String, Object> getMapRequiredInsertExtendedWidthRestricted() {
-
-			return new HashMap<>() {
-				{
-					put(dao.ATTR_ID, "1");
-					put(dao.ATTR_NAME, "Hotel 23");
-					put(dao.ATTR_CITY, "Vigo");
-					put(dao.ATTR_STATE, "Galicia");
-					put(dao.ATTR_COUNTRY, "ES");
-					put(dao.ATTR_PHONE, "+34 986 111 111");
-					put(dao.ATTR_EMAIL, "hotel1@atomicHotels.com");
-					
-				}
-			};
-		}
-
-		Map<String, Object> getMapRequiredDeletetExtendedWidthRestricted() {
-			return getMapRequiredInsertExtendedWidthRestricted();
-		}
-
-		HashMap<String, Object> getMapId() {
-			HashMap<String, Object> filters = new HashMap<>() {
-				{
-					put(dao.ATTR_ID, 1);
-				}
-			};
-			return filters;
+	Map<String, Object> getMapRequiredInsert() {
+		return new HashMap<>() {
+			{
+				put(dao.ATTR_NAME, "Hotel 23");
+				put(dao.ATTR_CITY, "Vigo");
+				put(dao.ATTR_STATE, "Galicia");
+				put(dao.ATTR_COUNTRY, "ES");
+			}
 		};
+	}
+
+	Map<String, Object> getMapUpdate() {
+		return getMapRequiredInsert();
+	}
+
+	Map<String, Object> getMapRequiredInsertExtended() {
+
+		return new HashMap<>() {
+			{
+				put(dao.ATTR_NAME, "Hotel 23");
+				put(dao.ATTR_CITY, "Vigo");
+				put(dao.ATTR_STATE, "Galicia");
+				put(dao.ATTR_COUNTRY, "ES");
+				put(dao.ATTR_PHONE, "+34 986 111 111");
+				put(dao.ATTR_EMAIL, "hotel1@atomicHotels.com");
+			}
+		};
+	}
+
+	Map<String, Object> getMapRequiredInsertExtendedWidthRestricted() {
+
+		return new HashMap<>() {
+			{
+				put(dao.ATTR_ID, "1");
+				put(dao.ATTR_NAME, "Hotel 23");
+				put(dao.ATTR_CITY, "Vigo");
+				put(dao.ATTR_STATE, "Galicia");
+				put(dao.ATTR_COUNTRY, "ES");
+				put(dao.ATTR_PHONE, "+34 986 111 111");
+				put(dao.ATTR_EMAIL, "hotel1@atomicHotels.com");
+
+			}
+		};
+	}
+
+	Map<String, Object> getMapRequiredDeletetExtendedWidthRestricted() {
+		return getMapRequiredInsertExtendedWidthRestricted();
+	}
+
+	HashMap<String, Object> getMapId() {
+		HashMap<String, Object> filters = new HashMap<>() {
+			{
+				put(dao.ATTR_ID, 1);
+			}
+		};
+		return filters;
+	};
 
 	List<String> getColumsName() {
 		List<String> columns = new ArrayList<>() {
