eaf9ac655d066055880f6a3f73ad705786e92aaf
Cesar Bouzas <cesarbouzaspaw@gmail.com>
2022-08-11 10:51:58 +0200

    Update PictureService.java

--- atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/service/PictureService.java
+++ atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/service/PictureService.java
@@ -2,22 +2,17 @@ package com.ontimize.atomicHotelsApiRest.model.core.service;
 
 
 
-import java.awt.Image;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.Iterator;
+
 import java.util.List;
 import java.util.Map;
 
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReadParam;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.ImageInputStream;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -42,24 +37,8 @@ public class PictureService implements IPictureService {
 	private PictureDao pictureDao;
 	@Autowired
 	private DefaultOntimizeDaoHelper daoHelper;
-
 	@Autowired
 	ControlFields cf;
-
-	private Image imageCoverter(byte[] bytes) throws IOException {
-		
-		
-		  ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
-		    Iterator readers = ImageIO.getImageReadersByFormatName("jpeg");    
-		    ImageReader reader = (ImageReader) readers.next();
-		    Object source = bis;
-		    ImageInputStream iis = ImageIO.createImageInputStream(source);
-		    reader.setInput(iis, true);
-		    ImageReadParam param = reader.getDefaultReadParam();
-		    return reader.read(0, param);
-		
-	}
-
 
 	@Override
 	public EntityResult pictureQuery(Map<String, Object>filter, List<String> columns)
@@ -68,16 +47,18 @@ public class PictureService implements IPictureService {
 		EntityResult resultado = new EntityResultWrong();
 		resultado=this.daoHelper.query(pictureDao, filter, columns);
 		Path p2=Paths.get("c:\\"+resultado.getRecordValues(0).get(pictureDao.ATTR_NAME)+"2.jpg" );
-		byte[] archivo=((String)(resultado.getRecordValues(0).get(pictureDao.ATTR_FILE))).getBytes();
 		
-		try {
-			Files.write(p2, archivo,StandardOpenOption.CREATE) ;
-		} catch (IOException e) {
-			// TODO Bloque catch generado automáticamente
-			e.printStackTrace();
-		}
-		
-		
+		System.out.println(resultado.getRecordValues(0));
+		resultado.getRecordValues(0).get(pictureDao.ATTR_FILE);
+//		byte[] bytes=(byte[]) resultado.getRecordValues(0).get(pictureDao.ATTR_FILE);
+//		
+//		try {
+//			Files.write(p2, bytes,StandardOpenOption.CREATE_NEW);
+//		} catch (IOException e) {
+//			// TODO Bloque catch generado automáticamente
+//			e.printStackTrace();
+//		}
+	
 		return resultado;
 	}
 
@@ -98,8 +79,6 @@ public class PictureService implements IPictureService {
 		cf.setRequired(required);
 		cf.validate(data);
 		
-		
-		
 			if (Files.exists(p)) {
 				data.put(pictureDao.ATTR_FILE, Files.readAllBytes(p));
 				resultado = daoHelper.insert(pictureDao, data);
@@ -114,24 +93,17 @@ public class PictureService implements IPictureService {
 			e.printStackTrace();
 			resultado=new EntityResultWrong(ErrorMessage.ERROR);
 		}
-
 		return resultado;
 	}
-
-
-
 	@Override
 	public EntityResult pictureDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException {
 		// TODO Esbozo de método generado automáticamente
 		return null;
 	}
-
-
 	@Override
 	public EntityResult pictureUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
 			throws OntimizeJEERuntimeException {
 		// TODO Esbozo de método generado automáticamente
 		return null;
 	}
-
 }
