5ff93e6144e4271c6133eb92e20707323af81290
Arlumno <arlumno@gmail.com>
2022-08-24 19:02:06 +0200

    no message

--- atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/tools/EntityResultWrong.java
+++ atomicHotelsApiRest-model/src/main/java/com/ontimize/atomicHotelsApiRest/model/core/tools/EntityResultWrong.java
@@ -1,4 +1,7 @@
 package com.ontimize.atomicHotelsApiRest.model.core.tools;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.dto.EntityResultMapImpl;
 
@@ -8,8 +11,15 @@ public class EntityResultWrong extends EntityResultMapImpl{
 	public EntityResultWrong() {
 		super(EntityResult.OPERATION_WRONG,EntityResult.NODATA_RESULT);
 	}
+	public EntityResultWrong(HashMap h) {
+		super(EntityResult.OPERATION_WRONG,EntityResult.NODATA_RESULT);
+		if (h != null) {
+            this.data = (Map) h.clone();
+        }
+	}
 	public EntityResultWrong(String resultMessage) {
 		super(EntityResult.OPERATION_WRONG,EntityResult.NODATA_RESULT,resultMessage);
 	}
+	
 
 }
